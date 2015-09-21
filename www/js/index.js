010d878021f1b2d44f71e259b517f17e9dd9aac3
GathoMauricio <mauricio2769@gmail.com>
2015-09-20 21:33:50 -0500

    sonido

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -27,6 +9,7 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        playAudio('sound/latido.mp3');
     },
     // deviceready Event Handler
     //
@@ -37,15 +20,22 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
         console.log('Received Event: ' + id);
     }
 };
-
 app.initialize();
+function playAudio(url) {
+        // Reproduce el fichero de audio de la URL
+        var my_media = new Media(url,
+            // FunciÃ³n 'callback' satisfactoria
+            function() {
+                console.log("playAudio():Audio correcto");
+            },
+            // FunciÃ³n 'callback' de error 
+            function(err) {
+                console.log("playAudio():Audio Error: "+err);
+        });
+
+        // Reproduce el audio
+        my_media.play();
+    }
